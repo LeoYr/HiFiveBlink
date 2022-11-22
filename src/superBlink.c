88faa4b71b29f9a45728650cd70f418f2fa5e603
Leo Yerena <yerenaleoram@gmail.com>
2022-11-22 09:33:49 -0600

    C code

--- src/superBlink.c
+++ src/superBlink.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "superBlink.h"
 #include <time.h>
-// #include <metal/gpio.h>
+#include <metal/gpio.h>
 
 //void setupGPIO(){
 
@@ -18,27 +18,49 @@
 //void blinkInBoardRGB();
 
 int main(){
-    int error = 0;
+
     int ledNum = 0;
-    int color[NUM_LEADS] = {GREEN_LED, RED_LED, BLUE_LED};
+    //int color[NUM_LEADS] = {GREEN_LED, RED_LED, BLUE_LED};
     int number[NUM_LEADS]={ZERO_LED,ONE_LED,TWO_LED,THREE_LED,FOUR_LED,FIVE_LED,SIX_LED,SEVEN_LED,EIGHT_LED,NINE_LED};
 
-    setupGPIO();
-
-    
-    while(!error){
+    //setupGPIO();
+   
+    while(ledNum > 20){
         
-        setLED(color[ledNum], ON);
+        //setLED(color[ledNum], ON);
         setLED(number[ledNum], ON);
         
         delay(DELAY);
-        error = setLED(color[ledNum], OFF);
-        error = setLED(number[ledNum], OFF);
+        //setLED(color[ledNum], OFF);
+        
+        setLED(number[ledNum], OFF);
         ledNum ++;
-        if(ledNum >= NUM_LEADS)
+        if(ledNum >= 10)
             ledNum = 0;
         //delay(DELAY);
 
     }
 
+
+    struct metal_gpio* gpio_device = metal_gpio_get_device(0);
+
+    metal_gpio_enable_output(gpio_device, 2);
+    metal_gpio_enable_output(gpio_device, 3);
+    metal_gpio_enable_output(gpio_device, 4);
+    metal_gpio_enable_output(gpio_device, 5);
+    metal_gpio_disable_pinmux(gpio_device, 2);
+    metal_gpio_disable_pinmux(gpio_device, 3);
+    metal_gpio_disable_pinmux(gpio_device, 4);
+    metal_gpio_disable_pinmux(gpio_device, 5);
+
+
+    metal_gpio_set_pin(gpio_device, 2, 1);
+    metal_gpio_set_pin(gpio_device, 3, 1);
+    metal_gpio_set_pin(gpio_device, 4, 1);
+    metal_gpio_set_pin(gpio_device, 5, 1);
+    
+    metal_gpio_enable_output(gpio_device, 1);
+    metal_gpio_disable_pinmux(gpio_device, 1);
+    metal_gpio_set_pin(gpio_device, 1, 0);
+    
 }
